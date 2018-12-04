9042897c800fc43e105621eb69f16a56ca06c8b2
dz-paji <zhangqian2002@outlook.com>
2018-12-05 07:30:17 +0800

    update test tick

--- app/index/config.php
+++ app/index/config.php
@@ -3,7 +3,7 @@ return [
     'app' => [
         'addpass' => true, //公共添加 true为指定用户可添加 false 为所有人都可以添加 不过需要审核
         'adduid' => "1", //添加功能用户UID [,]分隔 如 1,3,5
-        'testtick' => 300, //命令行定时执行时间 秒为单位 60*60 即3600秒
+        'testtick' => 600, //命令行定时执行时间 秒为单位 60*60 即3600秒
         'host' => 'https://vps.honoka.club', //这里修改成自己的域名
         'tgchannel' => 'https://t.me/luzhujichannel', //telegram 频道地址
         'tgchannelsckey' => '8009+OrTUX2oY4K/X5GfAPsFyF+S+6OCUQLo7uUfUkyk3sPvnZZmyK986w', // 邀请 https://t.me/HonokaVPS_StockBot 进入频道 设置为管理员 然后在频道发送 /start 获取sckey 填入

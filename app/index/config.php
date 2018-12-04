9562613cf09b1d448e895b3fdd849b08506b1010
dz-paji <zhangqian2002@outlook.com>
2018-12-04 16:54:17 +0800

    真香

--- app/index/config.php
+++ app/index/config.php
@@ -5,8 +5,8 @@ return [
         'adduid' => "1", //添加功能用户UID [,]分隔 如 1,3,5
         'testtick' => 60 * 60, //命令行定时执行时间 秒为单位 60*60 即3600秒
         'host' => 'https://vps.honoka.club', //这里修改成自己的域名
-        //'tgchannel' => 'https://t.me/luzhujichannel', //telegram 频道地址
-        //不再需要'tgchannelsckey' => 'fyuercvbfuferfyeuerbucu', // 邀请https://t.me/VpsStockBot进入频道 设置为管理员 然后在频道发送 /start 获取sckey 填入
+        'tgchannel' => 'https://t.me/luzhujichannel', //telegram 频道地址
+        'tgchannelsckey' => 'fyuercvbfuferfyeuerbucu', // 邀请 https://t.me/HonokaVPS_StockBot 进入频道 设置为管理员 然后在频道发送 /start 获取sckey 填入
         'testdebug' => false, //是否输出调试信息到文件
         'captcha' => true,//启动验证码
         'channelid' => ''//頻道地址
